2d30d722f906d8408fabcdbfc69536388a9e5ddf
Lanvige Jiang <lanvige@gmail.com>
2011-09-10 01:12:23 +0800

    Add sample code of mustache.js

--- views/spark_view.js
+++ views/spark_view.js
@@ -1,23 +1,35 @@
 App.Views.Spark = Backbone.View.extend({
-    el: jQuery("#warpper"),
-
+    el: $("#wrapper"),
+    
     events: {
-        "click #btnStartLevelTest": "gotoPretest",
+        "click #btnStart": "gotoPretest",
     },
 
     initialize: function () {
         _.bindAll(this, "render", "gotoPretest");
+
         this.render();
     },
 
     render: function () {
         // alert(JSON.stringify(this.model));
-
+        
+        // defaine mo for using in $.ajax().
+        var mo = this.model.toJSON();
+        
         // how to using Mustache.js to as template to render?
+        $.ajax({
+            url: "templates/spark.mustache",
+            context: document.body,
+            success: function(template){
+                var html = Mustache.to_html(template, mo).replace(/^\s*/mg, '');
+                $("#wrapper").append(html);
+            }
+        });
         
         return this; // for chainable calls, like .render().el
     },
-
+    
     gotoPretest: function () {
         alert("go to pretest");
     }
